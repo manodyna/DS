d1a7a590bc14d90d7fde337c2db4bd034a0bce38
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2021-09-28 19:13:59 +0530

    singly linked circular list works

---
+++ circulardoublylinkedlist/addfunc.c
@@ -0,0 +1,41 @@
+// #include "struct.h"
+void insertFront(int d, NODE **head)
+{
+    NODE *temp;
+    temp = getNode();
+    temp->data = d;
+    NODE *last;
+    if (*head == NULL)
+    {
+        *head = temp;
+    }
+    else
+    {
+        last = (*head)->prev;
+        temp->next = *head;
+        // (*head)->prev->next=temp;
+        (*head)->prev = temp;
+        last->next = temp;
+        temp->prev = last;
+        // (*head)->prev = temp;
+        *head = temp;
+    }
+}
+void insertRear(int d, NODE **head)
+{
+    NODE *last;
+    NODE *temp;
+    temp = getNode();
+    temp->data = d;
+    if (*head == NULL)
+    {
+        *head = temp;
+    }
+    else
+    {
+        last = (*head)->prev;
+        temp->next = *head;
+        temp->prev = last;
+        (*head)->prev = temp;
+    }
+}
