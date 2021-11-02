49485dba14d046dcab4dec5f9ec0f88818d55cc6
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2021-11-02 09:11:34 +0530

    ..

--- dump.c
+++ dump.c
@@ -1,123 +1,95 @@
-#include<stdio.h>
-//    #include<conio.h>
-#include<ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define MAX 50
+typedef struct node{
+    int data;
+    struct node *llink, *rlink;
+    int rthread, lthread;
+}node;
 
-typedef struct stack
-{
-    int data[MAX];
-    int top;
-}stack;
+struct tree{
+    node *root;
+};
 
-int precedence(char);
-void init(stack *);
-int empty(stack *);
-int full(stack *);
-int pop(stack *);
-void push(stack *,int);
-int top(stack *);   //value of the top element
-void infix_to_postfix(char infix[],char postfix[]);
+typedef struct tree tree;
 
-int main()
-{
-    char infix[30],postfix[30];
-    printf("Enter an infix expression(Donot leave space between characters):");
-    gets(infix);
-    infix_to_postfix(infix,postfix);
-    printf("\nPostfix expression: %s",postfix);
-    return 0;
+node *initialize(){
+    tree *new=malloc(sizeof(tree));
+    new->root=NULL;
+    return (new);
 }
 
-void infix_to_postfix(char infix[],char postfix[])
-{
-    stack s;
-    char x,token;
-    int i,j;    //i-index of infix,j-index of postfix
-    init(&s);
-    j=0;
-
-    for(i=0;infix[i]!='\0';i++)
-    {
-        token=infix[i];
-        if(isalnum(token))
-            postfix[j++]=token;
-        else
-            if(token=='(')
-               push(&s,'(');
-        else
-            if(token==')')
-                while((x=pop(&s))!='(')
-                      postfix[j++]=x;
-                else
-                {
-                    while(precedence(token)<=precedence(top(&s))&&!empty(&s))
-                    {
-                        x=pop(&s);
-                        postfix[j++]=x;
-                    }
-                    push(&s,token);
+void insert(tree *root, int ele){
+    node *temp = malloc(sizeof(node));
+    if(temp==NULL){
+        return;
+    }else{
+        temp->data=ele;
+        temp->rlink=temp->llink=NULL;
+        temp->rthread=temp->lthread=1;
+        node *cur=root;
+        if(cur==NULL){
+            tree->root->root=temp;
+            return;
+        }
+        while(1){
+            if(ele<cur->data){
+                if(cur->lthread==0){
+                    cur=cur->llink;
                 }
+                    else{
+                    break;
+                }
+            }
+
+            else if(ele>cur->data){
+                if(cur->rthread==0){
+                    cur=cur->rlink;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        if(ele<cur->data){
+            temp->llink=cur->llink;
+            temp->rlink=cur;
+            cur->lthread=0;
+            cur->llink=temp;
+
+        }   
+        else{
+            temp->rlink=cur->rlink;
+            temp->llink=cur;
+            cur->rlink=temp;
+            cur->rthread=0;
+        }
+
     }
+}
 
-    while(!empty(&s))
-    {
-        x=pop(&s);
-        postfix[j++]=x;
+node* inorder(node *root){
+    if(root==NULL){
+        printf("empty\n");
+        return;
+    }else if(root->lthread=1){
+        
     }
-
-    postfix[j]='\0';
 }
 
-int precedence(char x)
-{
-    if(x=='(')
-        return(0);
-    if(x=='+'||x=='-')
-        return(1);
-    if(x=='*'||x=='/'||x=='%')
-        return(2);
-
-    return(3);
+int main(){
+    int choice, data;
+    tree *root=initialize();
+    while(1){
+        printf("insert exit exit\n");
+        prinf("enter",&choice);
+        switch(choice){
+            case 1:
+            printf("data\n");
+            scanf("%d", &data);
+            inorder(root);
+        }
+    }
 }
-
-void init(stack *s)
-{
-    s->top=-1;
-}
-
-int empty(stack *s)
-{
-    if(s->top==-1)
-        return(1);
-
-    return(0);
-}
-
-int full(stack *s)
-{
-    if(s->top==MAX-1)
-        return(1);
-
-    return(0);
-}
-
-void push(stack *s,int x)
-{
-    s->top=s->top+1;
-    s->data[s->top]=x;
-}
-
-int pop(stack *s)
-{
-    int x;
-    x=s->data[s->top];
-    s->top=s->top-1;
-    return(x);
-}
-
-int top(stack *p)
-{
-    return (p->data[p->top]);
-}
-
